09836424b500c27d22f74e638488277a5e374d4d
Akronixx <josectp12yt@gmail.com>
2024-10-16 19:00:51 -0500

    Parte 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lol;
+            Console.WriteLine("Hola como te llamas?");
+            lol= Console.ReadLine();
+            Console.WriteLine($"Bienvenido {lol}");
+            Console.WriteLine("Desea agregar algo más?");
+        }
+    }
+}
